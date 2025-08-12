d208ecbfd2724834cc4784848a53ad3c768b3928
Vitor Oliva Coelho <vitorolivacoelho@gmail.com>
2025-08-12 18:24:51 -0300

    javascript adicionado

---
+++ assets/app.js
@@ -0,0 +1,58 @@
+let numeroSecreto = gerarNumeroAleatório();
+console.log(numeroSecreto);
+let tentativas = 1;
+
+function gerarNumeroAleatório(){
+    return parseInt(Math.random() * 10 + 1);
+}
+
+function alterarTexto(id, texto){
+    document.getElementById(id).innerText = texto;  
+}
+
+function verificarChute(){
+    console.log("O botão funcionou")
+    let chute = Number(document.querySelector('input').value);
+    let astroCerto = document.getElementById("astrocerto");
+    let astroErrado = document.getElementById("astroerrado");
+    let astro = document.getElementById("astro");
+    if(chute == numeroSecreto){
+        let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa"
+        alterarTexto('mensagem', `Parabéns`);
+        alterarTexto('dicas', `Você acertou o número com ${tentativas} ${palavraTentativa}`);
+        astroCerto.src = "../img/astronauta_certo.png";
+        astroCerto.style.display = "block";
+        astro.style.display = "none"
+        astroErrado.style.display = "none"
+    }else{
+        if(chute > numeroSecreto){
+            alterarTexto('dicas', `O número secreto é menor que ${chute}`);
+        }else{
+            alterarTexto('dicas', `O número secreto é maior que ${chute}`);
+        }
+        tentativas++
+        astroErrado.src = "../img/astronauta_errado.png";
+        astroErrado.style.display = "block";
+        astro.style.display = "none"
+    }
+}
+
+function limpar(){
+chute = document.querySelector('input');
+chute.value = '';
+let astroCerto = document.getElementById("astrocerto");
+let astroErrado = document.getElementById("astroerrado");
+let astro = document.getElementById("astro");
+astroCerto.style.display = "none"
+astro.style.display = "block"
+}   
+
+
+function reiniciarJogo(){
+    console.log("O botão funcionou");
+    numeroSecreto = gerarNumeroAleatório();
+    tentativas = 1
+    alterarTexto('mensagem', "Jogo do número secreto");
+    alterarTexto('dicas', "Escolha um número de 1 a 10");
+    limpar();
+}
